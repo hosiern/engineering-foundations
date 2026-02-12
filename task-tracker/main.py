fa6fa5601ea663ef47aa35332a9e7d3e235d45ca
Nathan Hosier Sr. <hosiern@msn.com>
2026-02-12 11:33:14 -0500

    feat: add program entry point and implement main menu loop

---
+++ task-tracker/main.py
@@ -0,0 +1,14 @@
+tasks = []
+
+while True:
+    print(f'*Task Tracker Main Menu*')
+    print('')
+    print(f'1- Create New Task')
+    print(f'2- Delete Task')
+    print(f'3- View Tasks')
+    print(f'4- Exit Program')
+    print('')
+    user_input = input('Enter an option: ')
+    print('')
+    
+    
